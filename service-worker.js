fa6229f90d97f65436c7c8a94defec23fa319403
GitHub <noreply@github.com>
2019-12-11 10:52:16 +0000

    bc93885 chore: upgrade headerbar to the new one (#167)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.812afbfeef5dec7124b0b0b0303f5c55.js"
+  "./precache-manifest.795febe8dac7c987f335197044837e59.js"
 );
 
 self.addEventListener('message', (event) => {
