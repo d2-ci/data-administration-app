a710c30f2554e7769ed75bc79806aca6c857a547
GitHub <noreply@github.com>
2022-02-07 14:54:51 +0000

    027fabf fix(data-statistics): render unlabeled rows (#724)

--- precache-manifest.fb0bf3eef5895375e0883834dc21b235.js
+++ precache-manifest.fb0bf3eef5895375e0883834dc21b235.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "24b779db108263e1957a21e3e365da5b",
+    "revision": "c10f41c50bf08b9cee75715936c23fd2",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.d5f24479.chunk.css"
   },
   {
-    "revision": "bc28b4d7ce8a798edfe5",
+    "revision": "86d3f1ee5e36bc0d0fc1",
     "url": "./static/css/main.43901c9c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.0174d36f.chunk.js"
   },
   {
-    "revision": "bc28b4d7ce8a798edfe5",
-    "url": "./static/js/main.1b2dbf22.chunk.js"
+    "revision": "86d3f1ee5e36bc0d0fc1",
+    "url": "./static/js/main.b4b669d3.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.77af4b981e613e0a007b529bb6fcd06f.js"
+  "./precache-manifest.fb0bf3eef5895375e0883834dc21b235.js"
 );
 
 self.addEventListener('message', (event) => {
